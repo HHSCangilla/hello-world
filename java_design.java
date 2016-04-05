1d9c7492431cca3ef6c274a97496655d246b2a7e
HHSCangilla <Cangilla@ht-sd.org>
2016-04-05 11:08:24 -0400

    Added files via upload

---
+++ java_design.java
@@ -0,0 +1,22 @@
+//Mr. Cangilla
+//Java
+//Purpose: Create a design
+
+import java.io.*;
+import static java.lang.System.*;  //set up stuff that we have to have
+
+class java_design{
+
+
+     public static void main (String str[]) throws IOException {
+
+          System.out.println("******-****-*********");
+          System.out.println("*******---***--*********");
+          System.out.println("********-----***********");
+          System.out.println("**/***************");
+          System.out.println("**\\*******8888888888888888*********");
+          System.out.println("**********+-+-********");
+          
+     }
+      
+}
